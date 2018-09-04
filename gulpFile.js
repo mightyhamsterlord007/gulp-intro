8ad9334a27efc424495696c4cb1c7c5b31ea96bf
Pak <Pak@admins-iMac-69.local>
2018-09-04 12:51:16 -0400

    add css task

--- gulpFile.js
+++ gulpFile.js
@@ -1,1 +1,20 @@
 var gulp = require('gulp');
+var ugliy = require('gulp-uglify');
+var minifyCss = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var livereload = require('gulp-livereload');
+var plumber = require('gulp-plumber');
+var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
+
+gulp.task('css', function() {
+
+    return gulp.src('public/css/**/*.css')
+        .pipe(minifyCss())
+        .pipe(concat('style.min.css'))
+        .pipe(gulp.dest('./public/dist/css/'))
+
+});
+
